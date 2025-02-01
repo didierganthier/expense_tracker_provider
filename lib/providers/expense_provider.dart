8f1688b8363a67aa7811b2dda0e4d4de834ce058
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-01-31 23:01:19 -0500

    Add initial project structure and configuration files for Flutter application

---
+++ lib/providers/expense_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:expense_tracker_provider/models/expense.dart';
+
+class ExpenseProvider extends ChangeNotifier {
+  final List<Expense> _expenses = [];
+
+  List<Expense> get expenses => _expenses;
+
+  void addExpense(String title, double amount, DateTime date, String category,
+      int priority) {
+    _expenses.add(
+      Expense(
+        id: const Uuid().v4(),
+        title: title,
+        amount: amount,
+        date: date,
+        category: category,
+        priority: priority,
+      ),
+    );
+    notifyListeners();
+  }
+}
